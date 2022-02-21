91c0a2a3ae316226edcf96ffce6c8a4dc7b95ed3
Richard444 <rlam3376@gmail.com>
2022-02-21 14:11:52 -0700

    part of user

---
+++ code/app/src/main/java/com/example/qrun/User.java
@@ -0,0 +1,37 @@
+package com.example.qrun;
+
+import java.util.ArrayList;
+
+public class User {
+    private int location;
+    private String username;
+    private ArrayList<Integer> QrCodes;
+    public User(int location, String username){
+        this.location = location;
+        this.username = username;
+    }
+
+    public int getLocation(){
+        return location;
+    }
+
+    public void setLocation(int location){
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void deleteQRcodes(int QrCode){
+        this.QrCodes.remove(QrCode);
+    }
+
+//    public void deleteUser(){
+//
+//    }
+}
