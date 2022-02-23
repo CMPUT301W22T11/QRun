1c25c4feffad549b6babed72b8e4587b5f706e5d
zxlim92 <limzixue@gmail.com>
2022-02-23 15:51:22 -0700

    User class merged

--- code/app/src/main/java/com/example/qrun/User.java
+++ code/app/src/main/java/com/example/qrun/User.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
 
 public class User {
     private ArrayList<QR> qrStore = new ArrayList<QR>() ;
-    private QR qrCode;
-    public void addQR(QR qr){
-        qrStore.add(qr)
+    private QR qrGameStatus;
+    private int location;
+    private String username;
+    private ArrayList<Integer> QrCodes;
+    public User(int location, String username){
+        this.location = location;
+        this.username = username;
     }
-    public ArrayList<QR> getQR(){
+    public void addQR(QR qr){
+        qrStore.add(qr);
+    }
+    public ArrayList<QR> getStoredQr(){
         return qrStore;
     }
     public void removeQR(QR qr){
@@ -22,11 +29,29 @@ public class User {
     public User getUser(){
         return this;
     }
-    public void setQR(QR qr){
-        qrCode = qr;
+    public void setQrGameStatus(QR qr){
+        qrGameStatus = qr;
     }
     public QR getQR(){
         return qrCode;
     }
+
+
+    public int getLocation(){
+        return location;
+    }
+
+    public void setLocation(int location){
+        this.location = location;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
 
 }
