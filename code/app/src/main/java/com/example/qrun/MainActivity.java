b5dac5f9772c513784ef91c9c52bcccfb67dd4e0
zxlim92 <limzixue@gmail.com>
2022-03-03 12:50:16 -0700

    mainscreen

--- code/app/src/main/java/com/example/qrun/MainActivity.java
+++ code/app/src/main/java/com/example/qrun/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.qrun;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
-    Button cameraButton;
+    ImageButton cameraBut;
     Button mapsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_screen);
-        cameraButton = findViewById(R.id.cameraButton);
-        mapsButton = findViewById(R.id.mapButton);
+        setContentView(R.layout.activity_main);
+
     }
+
+    public void nextPage(View v){
+        Intent intent = new Intent (this, MainScreen.class);
+        startActivity(intent);
+    }
+
 }
--- code/app/src/main/java/com/example/qrun/MainScreen.java
+++ code/app/src/main/java/com/example/qrun/MainScreen.java
@@ -3,12 +3,28 @@ package com.example.qrun;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainScreen extends AppCompatActivity {
-
+    ImageButton cameraBut;
+    Button mapsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+    }
+
+    public void cameraButton(View view){
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
     }
 }
