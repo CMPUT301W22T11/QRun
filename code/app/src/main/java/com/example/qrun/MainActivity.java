90898f38e47f5148f1a9a5af2aaaf79e8a3e7631
minh7a6 <minhdangle7a6@gmail.com>
2022-03-03 18:52:28 -0700

    some fixes to the login

--- code/app/src/main/java/com/example/qrun/MainActivity.java
+++ code/app/src/main/java/com/example/qrun/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.qrun;
 
+import android.os.Bundle;
+import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
 
-    @Override
+public class MainActivity extends AppCompatActivity implements SignupFragment.OnFragmentInteractionListener {
+    private Button Signup, Login;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Signup = findViewById(R.id.signup_button);
+        Login = findViewById(R.id.login_button);
+        Signup.setOnClickListener((l) -> {
+            new SignupFragment().show(getSupportFragmentManager(), "Signup");
+        });
+    }
+
+    @Override
+    public void onOkPressed(String name) {
+        if(name != null) {
+            
+            Log.d("onOkPressed", "User Id login" + name);
+        }
     }
 }
