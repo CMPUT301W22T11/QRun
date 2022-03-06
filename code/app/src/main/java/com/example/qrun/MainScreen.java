9485c24664c85b6700e0bb56e64ab554718778b6
minh7a6 <minhdangle7a6@gmail.com>
2022-03-06 12:58:06 -0700

    added show profile, as well as modify ScanningActivity to only do raw QR scanning

--- code/app/src/main/java/com/example/qrun/MainScreen.java
+++ code/app/src/main/java/com/example/qrun/MainScreen.java
@@ -1,7 +1,12 @@
 package com.example.qrun;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,6 +21,22 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.util.Map;
 
 public class MainScreen extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> ac = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    // if result is 1, then update the list
+                    if(result.getResultCode() == 1) {
+                        Log.d("add QR()", "ADD QR Successfully");
+                    }
+                    else {
+                        Log.d("add QR()", "Failed to Add QR");
+                    }
+                }
+            }
+    );
+    private Button showProfileBut; // TODO: Remove this when Zi's part complete
     ImageButton cameraBut;
     Button mapsButton;
     String userName;
@@ -24,27 +45,31 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
         Bundle extras = getIntent().getExtras();
+        cameraBut = findViewById(R.id.cameraButton);
+        showProfileBut = findViewById(R.id.temp); // TODO: Remove this when Zi's part complete
+        showProfileBut.setText("Remove This!!!!");// TODO: Remove this when Zi's part complete
         if(extras != null){
             userName = extras.getString("userName");
             Log.d("xx",userName);
         }
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         UserStorage userStorage = new UserStorage(db);
-
-
         userStorage.get(userName, (data)->{
-                    if(data!=null){
-                        String x = (String) data.get("email");
-                        Log.d("xx",x);
-                    }
-                }
-
-                );
-
-
-    }
-
-    public void cameraButton(View view){
+            if(data!=null){
+                String x = (String) data.get("email");
+                Log.d("xx",x);
+            }
+        });
+        cameraBut.setOnClickListener((l) -> {
+            Intent intent = new Intent(this, AddGameQR.class);
+            intent.putExtra("userName", userName);
+            ac.launch(intent);
+        });
+        showProfileBut.setOnClickListener((l) -> {
+            Intent intent = new Intent(this, ShowPlayerProfile.class);
+            intent.putExtra("userName", userName);
+            startActivity(intent);
+        });
 
     }
     @Override
--- code/app/src/main/java/com/example/qrun/QRGame.java
+++ code/app/src/main/java/com/example/qrun/QRGame.java
@@ -1,7 +1,12 @@
 package com.example.qrun;
 
+import android.util.Log;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.io.Serializable;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 /**
  * Class to represent Scanned QRGame codes as a form of
@@ -12,21 +17,66 @@ public class QRGame extends QR implements Serializable {
 
 
     private long points;
-
+    private double lat, lon;
+    private String path;
 
     /**
      * Constructor for QRGame takes in the text that the QRGame represents
      * The constructor uses this text to calculate the points of the QRGame
      * @param Text String that the QRGame code represents
      */
-    QRGame(String Text){
-
-        super(Text);
-
+    public QRGame(String Text, String username, double lat, double lon, String path){
+        super(Text, username);
         this.points = QRCalculation.calcScore(this.getHexString());
+        this.lat = lat;
+        this.lon = lon;
+        this.path = path;
+
+    }
+    public QRGame(String id) {
+        super(null, null);
+        QRStorage storage = new QRStorage(FirebaseFirestore.getInstance());
+        storage.get(id, (l) -> {
+            if(l != null) {
+                String username = (String)l.get("username");
+                String hexString = (String)l.get("hexString");
+                lat = Double.valueOf((String)l.get("latitude"));
+                lon = Double.valueOf((String)l.get("longitude"));
+                path = (String)l.get("PicPath");
+                points = Long.valueOf((String)l.get("points"));
+                this.hexString = hexString;
+                this.username = username;
+            }
+        });
     }
 
+    public void setPoints(long points) {
+        this.points = points;
+    }
 
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
 
     /**
      *
