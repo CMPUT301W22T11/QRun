5de6390da4dc1e4699dfccfa94568e9cc2cb0b7c
Lucheng Zhou <zhoulucheng4424@gmail.com>
2022-03-12 15:40:46 -0700

    intent test and //comment

--- code/app/src/main/java/com/example/qrun/QrContentActivity.java
+++ code/app/src/main/java/com/example/qrun/QrContentActivity.java
@@ -45,7 +45,7 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
     ArrayList<Comment> commentContentList;
     String QRid;
     FirebaseFirestore db;
-    //    FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 //    FirebaseUser user = mAuth.getCurrentUser();
     String uid ;//use user email as reference for now
 
@@ -62,15 +62,15 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
         commentAdapter = new CustomComment(this, commentContentList);
         commentList.setAdapter(commentAdapter);
         db = FirebaseFirestore.getInstance();
-        DocumentReference QrRef = db.collection("User").document(uid).collection("QrWallet").document(QRid);
+        DocumentReference QrRef = db.collection("User").document(uid).collection("QrWallet").document(QRid);//use subcollection to store owned qid
         Map<String, ArrayList<String>> comments = new HashMap<>();
-        final FloatingActionButton addCommentButton = findViewById(R.id.add_comment_button);
+        final FloatingActionButton addCommentButton = findViewById(R.id.add_comment_button);//float button to add comment
 
-        addCommentButton.setOnClickListener(new View.OnClickListener() {
+        addCommentButton.setOnClickListener(new View.OnClickListener() {//pop up dialog when float button pressed
             @Override
             public void onClick(View view) {
                 DialogFragment addComment = new AddCommentFragment();
-                Bundle currentQr = new Bundle();
+                Bundle currentQr = new Bundle();//use bundle to pass current Q hash into add comment fragment to generate comment instance
                 currentQr.putString("currentQr",QRid);
                 addComment.setArguments(currentQr);//send current qid into addCommentFragment
                 addComment.show(getSupportFragmentManager(),"add_comment");
@@ -82,38 +82,37 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
             public void onEvent(@Nullable DocumentSnapshot snapshot,
                                 @Nullable FirebaseFirestoreException e) {
 
-                commentContentList.clear();
+                commentContentList.clear();                     //clear local data list
                 ArrayList<String> commentText = new ArrayList<>();
-                commentText = (ArrayList<String>) snapshot.getData().get("comments");
+                commentText = (ArrayList<String>) snapshot.getData().get("comments");//get data from cloud
                 for(int i=0;i<commentText.size();i++){
                     Comment comment = new Comment(QRid, uid,commentText.get(i));
-                    commentContentList.add(comment);
-                    Log.d("comments", commentText.get(i));
+                    commentContentList.add(comment);                    //append cloud data into local data list
                 }
-                commentAdapter.notifyDataSetChanged();
+                commentAdapter.notifyDataSetChanged();                  //update array adapter
             }
         });
 
 
-        commentList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {//set up long click to delete selected comment
+        commentList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {      //set up long click to delete selected comment
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int item, long l) {
-                new AlertDialog.Builder(QrContentActivity.this)
+                new AlertDialog.Builder(QrContentActivity.this)                         //When long click, pop up a dialog to prompt delete
                         .setMessage("Do you want to delete the String?")
                         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int i) {
-                                if (uid != commentContentList.get(item).getUid()) {//check the whether user is trying to delete others' comment
+                                if (uid != commentContentList.get(item).getUid()) {             //check the whether user is trying to delete others' comment
                                     Toast.makeText(getApplicationContext(), "You can't delete other's comment", Toast.LENGTH_SHORT).show();
                                 } else {
-                                    ArrayList<String> tempCommentList = new ArrayList<>();
-                                    commentContentList.remove(item);
-                                    for(int j=0;j<commentContentList.size();j++){
+                                    ArrayList<String> tempCommentList = new ArrayList<>();      //a temporary container for comment text
+                                    commentContentList.remove(item);//remove selected item form data list
+                                    for(int j=0;j<commentContentList.size();j++){               //use for loop to append all remaining comment text into temp container
                                         tempCommentList.add(commentContentList.get(j).getComment());
                                     }
-                                    comments.put("comments", tempCommentList);
+                                    comments.put("comments", tempCommentList);               //put into hash map
                                     QrRef
-                                            .set(comments)
+                                            .set(comments)//update firebase
                                             .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                 @Override
                                                 public void onSuccess(Void aVoid) {
@@ -131,7 +130,7 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
                                 }
                             }
                         })
-                        .setNegativeButton("No" , null).show();
+                        .setNegativeButton("No" , null).show();             //cancel deletion when No clicked
                 return true;
             }
         });
@@ -139,14 +138,14 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
          * This listener will bring up a dialog contains the comment user want to view
          * @author: Lucheng Zhou
          */
-        commentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        commentList.setOnItemClickListener(new AdapterView.OnItemClickListener() {  //show a dialog with comment text in it when short clicked
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 tempComment = commentAdapter.getItem(i);
                 DialogFragment viewComment = new CommentViewFragment();
-                Bundle commentBundle = new Bundle();
+                Bundle commentBundle = new Bundle();        //bundle selected comment text
                 commentBundle.putString("Comment", tempComment.getComment());
-                viewComment.setArguments(commentBundle);//these function will send selected comment text to view fragment
+                viewComment.setArguments(commentBundle);    //these function will send selected comment text to view fragment
                 viewComment.show(getSupportFragmentManager(),"view_comment");
             }
         });
@@ -156,15 +155,15 @@ public class QrContentActivity extends AppCompatActivity implements AddCommentFr
 
     @Override
     public void onOkPressed(Comment newComment) {
-        ArrayList<String> tempCommentList = new ArrayList<>();
-        commentContentList.add(newComment);
-        for(int i=0;i<commentContentList.size();i++){
+        ArrayList<String> tempCommentList = new ArrayList<>();          //set up a temp container for comment text
+        commentContentList.add(newComment);                             //add new comment object into local data list
+        for(int i=0;i<commentContentList.size();i++){                   //use for loop to extract comment text into temp container
             tempCommentList.add(commentContentList.get(i).getComment());
         }
         Map<String, ArrayList<String>> comments = new HashMap<>();
-        comments.put("comments",tempCommentList);
+        comments.put("comments",tempCommentList);                   //put comment text into hashmap
         DocumentReference QrRef = db.collection("User").document(uid).collection("QrWallet").document(QRid);
-        QrRef
+        QrRef                                                           //push data into cloud
                 .set(comments)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
