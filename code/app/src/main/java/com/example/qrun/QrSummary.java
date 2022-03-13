bc891f0016069ba2f0e3089cfcfec5ad4736f32d
minh7a6 <minhdangle7a6@gmail.com>
2022-03-13 15:00:48 -0600

    changed some stuff for migration

--- code/app/src/main/java/com/example/qrun/QrSummary.java
+++ code/app/src/main/java/com/example/qrun/QrSummary.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
+import com.example.qrun.Fragment.*;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
