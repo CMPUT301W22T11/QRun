3567cd9812a6368341311e4d51ea060353c15052
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2022-03-31 15:49:01 -0600

    Add files via upload

--- code/app/src/main/java/com/example/qrun/QRDeleteActivity.java
+++ code/app/src/main/java/com/example/qrun/QRDeleteActivity.java
@@ -2,12 +2,18 @@ package com.example.qrun;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import com.example.qrun.Fragment.DeleteQRFragment;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -23,7 +29,6 @@ public class QRDeleteActivity extends AppCompatActivity {
     private ArrayList<String> QRListNames=new ArrayList<String>();
 
     private ArrayList<QRGame> QRList=new ArrayList<>();
-
     private QRStorage QRstorage;
 
     private String QRName;
@@ -37,9 +42,10 @@ public class QRDeleteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_qrdelete);
-
+        EditText SearchQR=findViewById(R.id.editTextTextPersonName);
+        Button deleteBut=findViewById(R.id.button2);
         QRListView=findViewById(R.id.QRListView);
-
+        Button searchBut=findViewById(R.id.SearchBut);
         QRAdapter=new ArrayAdapter<String>(this,R.layout.qr_lists_content,QRListNames);
 
         QRListView.setAdapter(QRAdapter);
@@ -50,18 +56,19 @@ public class QRDeleteActivity extends AppCompatActivity {
         {
             getData(queryDocumentSnapshots);
         });
+
+        //here it is the listView of QRs and if clicked, the QR will delete
         QRListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 QRListNames.remove(i);
-
                 QRAdapter.notifyDataSetChanged();
-
                 QRGame temp=QRList.get(i);
-
                 QRstorage.delete(temp,comp);
+                QRList.remove(i);
             }
         });
+
     }
     //get the data from database
     private void getData(QuerySnapshot queryDocumentSnapshots) {
@@ -75,11 +82,12 @@ public class QRDeleteActivity extends AppCompatActivity {
                     QRListNames.add(new QRGame(document).getHexString());
 
                     QRList.add(new QRGame(document));
+
                 }
 
             }
 
-            // remove those QRs with same hexString
+//             remove those QRs with same hexString
             for(int i=0;i<QRListNames.size();i++){
                 String key=QRListNames.get(i);
                 int j=i+1;
@@ -97,4 +105,5 @@ public class QRDeleteActivity extends AppCompatActivity {
         }
 
     }
+
 }
