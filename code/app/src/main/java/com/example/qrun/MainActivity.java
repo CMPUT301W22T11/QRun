281e86876cc0ba6f669352ad3522732100112c6c
minh7a6 <minhdangle7a6@gmail.com>
2022-04-02 12:58:22 -0600

    added logout, factor signup fragment

--- code/app/src/main/java/com/example/qrun/MainActivity.java
+++ code/app/src/main/java/com/example/qrun/MainActivity.java
@@ -1,10 +1,8 @@
 package com.example.qrun;
 
-import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.activity.result.ActivityResult;
@@ -13,18 +11,13 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.qrun.Fragment.SignupFragment;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.io.File;
 
 /**
  * The Screen Before Login/Signup
