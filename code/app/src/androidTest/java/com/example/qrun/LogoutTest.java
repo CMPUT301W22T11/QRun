98b05d75894f9eb7c876a4653961f10bdb82a339
YouweiPeng <98921666+YouweiPeng@users.noreply.github.com>
2022-04-04 11:53:42 -0600

    Update LogoutTest.java

--- code/app/src/androidTest/java/com/example/qrun/LogoutTest.java
+++ code/app/src/androidTest/java/com/example/qrun/LogoutTest.java
@@ -14,7 +14,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
+//The test requires the device must has been login before
 public class LogoutTest {
     private Solo solo;
     FirebaseFirestore db;
